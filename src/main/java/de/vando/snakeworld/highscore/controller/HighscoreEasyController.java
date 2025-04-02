e43ec9413d3b970387ef33d0d2a3b5ededcd1414
vando02800 <vando.02800@gmail.com>
2025-04-02 14:23:42 +0200

    routes updated

--- src/main/java/de/vando/snakeworld/highscore/controller/HighscoreEasyController.java
+++ src/main/java/de/vando/snakeworld/highscore/controller/HighscoreEasyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/highscore/easy")
+@RequestMapping("/highscore/leicht")
 public class HighscoreEasyController {
 
     @Autowired
--- src/main/java/de/vando/snakeworld/highscore/controller/HighscoreNormalController.java
+++ src/main/java/de/vando/snakeworld/highscore/controller/HighscoreNormalController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/highscore/normal")
+@RequestMapping("/highscore/mittel")
 public class HighscoreNormalController {
 
     @Autowired
